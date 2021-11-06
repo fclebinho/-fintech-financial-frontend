f73390d44a93b4797780672118ba23873f8e979f
Cleber Gomes <cleber.gomes@govoll.com>
2021-11-06 08:50:52 -0300

    Ajustes

--- src/service/create-apollo.ts
+++ src/service/create-apollo.ts
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 export const createApolloClient = (authToken: String) => {
   return new ApolloClient({
     link: new HttpLink({
-      uri: 'https://staging-graphql-gateway.herokuapp.com/graphql',
+      uri: 'https://staging-financial-api.herokuapp.com/graphql',
       headers: {
         Authorization: `Bearer ${authToken}`
       }
